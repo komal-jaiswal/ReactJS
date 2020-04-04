99a25720827c5e547e3de0f7f2f34e22977c600f
komal-jaiswal <jaiswalkomal18@gmail.com>
2020-04-04 23:31:50 +0530

    multiple setState call

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,21 +8,36 @@ class Counter extends Component {
         }
     }
     increment() {
+        // this.setState({
+        //     count: this.state.count + 1
+        // },
+        //     () => {
+        //         //this callback is needed if you have to do some operation after button clicked
+        //         console.log('callback function' + this.state.count)
+        //     }
+        // )
+        // console.log(this.state.count)
+
+        //to demostarate the multiple statecall
         this.setState({
             count: this.state.count + 1
         },
             () => {
-                //this callback is needed if you have to do some operation after button clicked
-                console.log('callback function' + this.state.count)
-            }
-        )
-        console.log(this.state.count)
+                console.log("Callback count" + this.state.count)
+            })
+    }
+    incrementfive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
     render() {
         return (
             <div>
                 {this.state.count}
-                <button onClick={() => this.increment()}>Increment</button>
+                <button onClick={() => this.incrementfive()}>Increment</button>
             </div>
         )
     }
